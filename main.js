83a075aed6758f0e1b5bf29e9da15859d0f34717
Xinyi Jiang <magicalginger@gmail.com>
2022-11-23 03:14:42 +1100

    add clock and date

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+//testing code to see if main.js is linked
 console.log('main.js is running.')
+//display current date and time on page
+function myClock() {         
+    setTimeout(function() {   
+      const d = new Date();
+      document.getElementById("dateContainer").innerHTML = `&#128198 Date: ${d.toLocaleDateString(
+        'default', {weekday: 'short'})} ${d.toLocaleDateString()}`; 
+      document.getElementById("clockContainer").innerHTML = `&#8986 Time: ${d.toLocaleTimeString()}`; 
+      myClock();             
+    }, 1000)
+  }
+  myClock();    
