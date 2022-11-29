65db25cee3dba542a721b52ba2cbdb1a52316778
Jiazhen Weng <114340093+jiazhen-weng@users.noreply.github.com>
2022-11-29 10:50:08 +1030

    mark as done feature on UI, data update coming soon

--- render.js
+++ render.js
@@ -22,32 +22,25 @@ function createTaskHTML(task) {
             break;
     }
 
-    return `<div class="col-12 col-sm-6 col-lg-3 col-xl-2 mx-1" style="margin:2em !important;">
+    return `<div class="col-12 col-sm-6 col-lg-3 col-xl-2 mx-1" style="margin:2em !important;">    
                 <div class="card mx-0" style="width: 15rem; background-color: lightsteelblue; border:none; max-width:300px;">
-                    <div class="card-body mx-auto" style="background:url(${stickyNote}) no-repeat; background-size:cover; border: none;">
+                    <div class="card-body mx-auto" style="background-image: url(${stickyNote});" id="card-body-${task.id}">
                         <div id="card-content-wrapper">
-                        <h5 class="card-title">&#x1F4CC ${task.name}</h5>
-                        <p class="card-text">${task.description}</p>
-                        <p class="card-text">Assigned to: ${assigned}</p>
-                        <p class="card-text">Due on: ${task.dueDate}</p>
-                        <p class="card-text">Status: ${task.status} </p>
-                        <br>
+                            <h5 class="card-title">&#x1F4CC ${task.name}</h5>
+                            <p class="card-text">${task.description}</p>
+                            <p class="card-text">Assigned to: ${assigned}</p>
+                            <p class="card-text">Due on: ${task.dueDate}</p>
+                            <p class="card-text">Status: ${task.status} </p>
+                            <br>
                         </div>
                         <!-- Button trigger modal -->
-                        <button type="button" class="card-button btn btn-outline-success" data-toggle="modal"  id="doneBtn-${task.id}">
-                        Mark as done
-                        </button>
+                        <button type="button" class="card-button btn btn-outline-success" id="doneBtn-${task.id}">Mark as done</button>
                         <br>
-                        <button type="button" class="card-button btn btn-info" data-toggle="modal" data-target="#task${task.id}">
-                        View 
-                        </button>
-                        <button type="button" class="card-button btn btn-primary" data-toggle="modal" data-target="#addTaskModal" id="editBtn-${task.id}">
-                        Edit
-                        </button>
-                        <button type="button" class="card-button btn btn-danger id="deleteBtn-${task.id}">
-                        Delete
-                        </button>
-                        <div class="modal fade" id="task${task.id}" tabindex="-1" role="dialog"
+                        <button type="button" class="card-button btn btn-info" data-toggle="modal" data-target="#task${task.id}">View</button>
+                        <button type="button" class="card-button btn btn-primary" data-toggle="modal" data-target="#addTaskModal" id="editBtn-${task.id}">Edit</button>
+                        <button type="button" class="card-button btn btn-danger" id="deleteBtn-${task.id}">Delete</button>
+                    </div>
+                    <div class="modal fade" id="task${task.id}" tabindex="-1" role="dialog"
                              aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
                             <div class="modal-dialog modal-dialog-centered" role="document">
                                 <div class="modal-content">
@@ -103,7 +96,6 @@ function createTaskHTML(task) {
                                 </div>
                             </div>
                         </div>
-                    </div>
                 </div>
             </div>`;
 }
