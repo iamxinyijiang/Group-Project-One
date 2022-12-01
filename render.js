a95e2b269b92776195b5a67760f4ab27290f8631
Xinyi Jiang <magicalginger@gmail.com>
2022-12-01 22:30:26 +1100

    tweak column width

--- render.js
+++ render.js
@@ -31,7 +31,7 @@ function createTaskHTML(task) {
             break;
     }
 
-    return `<div class="col-12 col-sm-6 col-lg-3 col-xl-2 mx-1" style="margin:2em !important; order: ${flexOrder}" id="cardWrapper-${task.id}">    
+    return `<div class="col-12 col-sm-6 col-lg-2 mx-5" style="margin:2em !important; order: ${flexOrder}" id="cardWrapper-${task.id}">    
                 <div class="card mx-0" style="width: 15rem; background-color: lightsteelblue; border:none; max-width:250px;">
                     <div class="card-body mx-auto" style="background-image: url(${stickyNote});" id="card-body-${task.id}">
                         <div id="taskDetail-${task.id}">
